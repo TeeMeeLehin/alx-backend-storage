b7a0e618925c15ca6264b7ed0b2c7bae0da69912
TeeMeeLehin <samemerald8@gmail.com>
2024-01-24 23:40:28 +0100

    redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,9 +2,9 @@
 """ writing strings to redis"""
 import redis
 import uuid
-from typing import List, Union
+from typing import List, Union, Callable, Optional
 
-mlist = Union[str, bytes, int, float]
+mlist = Union[str, bytes, int, float, None]
 
 
 class Cache():
@@ -19,3 +19,19 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> mlist:
+        """get func"""
+        data = self._redis.get(key)
+        if data is not None:
+            return fn(data) if fn else data
+        return None
+
+    def get_str(self, key: str) -> Optional[str]:
+        """func to convert data to str"""
+        return self.get(key, fn=lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """func to convert data to int
+        """
+        return self.get(key, fn=int)
